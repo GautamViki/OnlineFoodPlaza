9f43709f7619bf7730d014519a246ed5e4de8cd8
Chandani99 <er.chandni99@gmail.com>
2022-08-12 12:45:11 +0530

    itemupdate

--- src/main/java/com/masai/entity/Item.java
+++ src/main/java/com/masai/entity/Item.java
@@ -20,83 +20,101 @@ public class Item {
 
 
 	@OneToOne
-	private List<Restaurant> restaurant;
+	private List<Restaurant> restaurants;
 
-	public Item(String itemId, String itemName, Category category, Integer quantity, Double cost, List<Restaurant> restaurants) {
-		this.itemId = itemId;
-		this.itemName = itemName;
-		this.category = category;
-		this.quantity = quantity;
-		this.cost = cost;
-		this.restaurants = restaurants;
-	}
+
 
 
 	public String getItemId() {
 		return itemId;
 	}
 
+
+
+
 	public void setItemId(String itemId) {
 		this.itemId = itemId;
 	}
+
+
+
 
 	public String getItemName() {
 		return itemName;
 	}
 
+
+
+
 	public void setItemName(String itemName) {
 		this.itemName = itemName;
 	}
+
+
+
 
 	public Category getCategory() {
 		return category;
 	}
 
+
+
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
+
+
+
 
 	public Integer getQuantity() {
 		return quantity;
 	}
 
+
+
+
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+
+
+
 
 	public Double getCost() {
 		return cost;
 	}
 
+
+
+
 	public void setCost(Double cost) {
 		this.cost = cost;
 	}
 
-	public List<Restaurant> getResturaunt() {
-		return restauraunt;
-	}
-	public void setRestauraunt(List<Restaurant> restauraunt) {
-		this.restauraunt = restauraunt;
+
 
 
 	public List<Restaurant> getRestaurants() {
 		return restaurants;
 	}
 
+
+
+
 	public void setRestaurants(List<Restaurant> restaurants) {
 		this.restaurants = restaurants;
-
 	}
+
+
+
 
 	@Override
 	public String toString() {
-		return "Item{" +
-				"itemId='" + itemId + '\'' +
-				", itemName='" + itemName + '\'' +
-				", category=" + category +
-				", quantity=" + quantity +
-				", cost=" + cost +
-				", restaurants=" + restaurants +
-				'}';
+		return "Item [itemId=" + itemId + ", itemName=" + itemName + ", category=" + category + ", quantity=" + quantity
+				+ ", cost=" + cost + ", restaurants=" + restaurants + "]";
 	}
+
+	
+	
 }
