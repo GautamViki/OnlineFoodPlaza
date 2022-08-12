4895f705ad5966e4c0285c422fd9bc13c0ed2ddc
brijeshkumarrai <brijeshrai9820@gmail.com>
2022-08-12 22:34:20 +0530

    implClass added

---
+++ src/main/java/com/masai/service/CustomerServiceImpl.java
@@ -0,0 +1,39 @@
+package com.masai.service;
+
+import java.util.List;
+
+import com.masai.entity.Customer;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public Customer addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer updateCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer removeCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer viewCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer viewAllCustomers(List<Customer> customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/masai/service/RestaurantServiceImpl.java
@@ -0,0 +1,45 @@
+package com.masai.service;
+
+import java.util.List;
+
+import com.masai.entity.Restaurant;
+
+public class RestaurantServiceImpl implements RestaurantService{
+
+	@Override
+	public Restaurant addRestaurant(Restaurant res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Restaurant updateRestaurant(Restaurant res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Restaurant removeRestaurant(Restaurant res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Restaurant viewRestaurant(Restaurant res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Restaurant viewBearByRestaurant(List<Restaurant> location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Restaurant viewRestaurantByItemName(List<Restaurant> name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
