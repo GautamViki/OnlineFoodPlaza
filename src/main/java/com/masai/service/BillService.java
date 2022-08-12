60dd7046007e0529319f056d53545bce7109bed3
GautamViki <vikacse@gmail.com>
2022-08-12 22:27:03 +0530

    bill,orderDetails,login service update

--- src/main/java/com/masai/service/BillService.java
+++ src/main/java/com/masai/service/BillService.java
@@ -4,7 +4,6 @@ import com.masai.entity.Bill;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
--- src/main/java/com/masai/service/OrderService.java
+++ src/main/java/com/masai/service/OrderService.java
@@ -3,8 +3,6 @@ package com.masai.service;
 import com.masai.entity.Customer;
 import com.masai.entity.OrderDetails;
 import com.masai.entity.Restaurant;
-import org.aspectj.weaver.ast.Or;
-import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
